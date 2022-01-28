71e284a36b9ce7d7e1a62b2b9229b0906f6b81d9
Fadhli Yulyanto <fadhliyulyanto@gmail.com>
2022-01-29 01:25:16 +0700

    blogs comment services

--- app/controller/blogs.js
+++ app/controller/blogs.js
@@ -1,5 +1,12 @@
 const db = require('../helper/database')
 const moment = require('moment')
+const { sha256Encryptor, sha256Generator } = require('../helper/encryptor')
+
+async function comments(id) {
+    const result = await db.query(`SELECT username, created_date as \"createdDate\", comment_text as \"commentText\" from exp_blog_posts_comments where post__id = $1`, [id])
+
+    return result.rows
+}
 
 async function lists(request, response) {
     try {
@@ -64,11 +71,19 @@ async function details(request, response) {
 
         const result = await db.query(`SELECT * FROM exp_blog_posts where id = $1`, [id])
 
+        const post = result.rows[0]
+        const getComments = await comments(id)
+
+        const data = {
+            ...post,
+            comments: getComments.rows
+        }
+
         if (result.rowCount > 0) {
             response.status(200).json({
                 status: 'success',
                 message: 'success',
-                results: result.rows[0]
+                results: data
             })
         } else {
             response.status(500).json({
@@ -84,8 +99,49 @@ async function details(request, response) {
     }
 }
 
+async function insertComments(request, response) {
+    const { postId } = request.params
+    const { securitycode, timestamp } = request.headers
+    const { username, commentText } = request.body
+
+    const hash = sha256Generator('encrypt', process.env.API_KEY + username + timestamp)
+    console.log({})
+
+    if (hash === securitycode) {
+        const results = await db.query(`INSERT INTO exp_blog_posts_comments(post__id, username, comment_text, created_date) values($1, $2, $3, $4)`, [postId, username, commentText, moment().format('YYYY-MM-DD HH:mm:ss')])
+
+        const getComments = await comments(postId)
+
+        if (results) {
+            response.status(200).json({
+                status: 'success',
+                message: 'success',
+                results: getComments
+            })
+        }
+    } else {
+        response.status(401).json({
+            status: 'error',
+            message: 'Your security code was invalid!'
+        })
+    }
+}
+
+async function getComments(request, response) {
+    const { id } = request.body
+    const commentResults = await comments(id)
+
+    response.status(200).json({
+        status: 'success',
+        message: 'success',
+        results: commentResults
+    })
+}
+
 module.exports = {
     insertBlogs,
     lists,
-    details
+    details,
+    insertComments,
+    getComments
 }
--- app/helper/router.js
+++ app/helper/router.js
@@ -12,6 +12,7 @@ router.use(function hitlog(request, response, next) {
 router.post('/authentication', auth.login)
 router.get('/blogs', blogs.lists)
 router.get('/blogs/details/:id', blogs.details)
+router.post('/blogs/comments/:postId', blogs.insertComments)
 
 router.use(checkSession)
 
